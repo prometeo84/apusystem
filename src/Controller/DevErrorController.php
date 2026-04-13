d9a85a42a48a7d598bfcc0fea62cc163caf90464
Andrés Bravo <andres.bravo@sercop.gob.ec>
2026-04-13 12:11:46 -0500

    test: add dev route to trigger exception for email notifications

---
+++ src/Controller/DevErrorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DevErrorController
+{
+    #[Route('/__trigger_error_for_test', name: 'app_trigger_error_for_test')]
+    public function trigger(): Response
+    {
+        // Lanzar una excepción deliberada para probar el listener
+        throw new \RuntimeException('Prueba: excepción forzada para validar el envío de email por ErrorNotificationListener');
+    }
+}
